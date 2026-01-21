18787efe9b8b8db97c4ade62fdbe553db30881cf
Luca Striano <luca.striano90@gmail.com>
2026-01-21 16:38:29 +0100

    creazione component header e relativo layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
+import MainHeader from "./components/MainHeader";
 
+
+function App() {
+  return (
+    <>
+      <MainHeader />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,11 @@
+function MainHeader() {
+    return (
+        <header>
+            <h1>Learn Web development</h1>
+        </header>
+
+
+    )
+}
+
+export default MainHeader
